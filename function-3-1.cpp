0dca78d36f359b5c15bf6171c7d0aa2b4220880d
HenryTang2702 <109932321+HenryTang2702@users.noreply.github.com>
2022-07-29 16:07:30 +0930

    update

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -3,13 +3,10 @@ bool is_fanarray(int array[], int n){
 
         return false;
     }
-    for(int i =1; i < n/2; i++){
-        if(array[i-1] < array[i] ){
-            return true;
-        }
-        else{
+    for(int i =1; i < n; i++){
+        if(array[i-1] > array[i] && array[i-1] != array[n-i] ){
             return false;
         }
-
     }
+    return true;
 }
