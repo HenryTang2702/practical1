8440484268082a83bbd80ab5783f2ae0f568a639
HenryTang2702 <109932321+HenryTang2702@users.noreply.github.com>
2022-07-29 16:14:10 +0930

    update

--- main-3-3.cpp
+++ main-3-3.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 double weighted_average(int array[], int n);
 int main(){
-
-    return 0;
+   int arr[6] = {1,2,3,1,1,2};
+   cout<<weightedAverage(arr, 6)<<endl;
+   return 0;
 }
